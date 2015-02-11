cb633656cd8d6a59d0e625465237283cc6f0affd
Henk van Boeijen <noreply@solidsoils.nl>
2015-02-11 14:07:47 +0100

    Integration Test project added.

---
+++ Solid.Arduino.IntegrationTest/SerialConnectionTester.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO.Ports;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Solid.Arduino;
+
+namespace Solid.Arduino.IntegrationTest
+{
+    /// <summary>
+    /// Performs tests with a connected Arduino device.
+    /// </summary>
+    [TestClass]
+    public class SerialConnectionTester
+    {
+        /// <summary>
+        /// Finds a live serial connection by issuing a Firmata SysEx Firmware query (0xF0 0x79 0xF7).
+        /// </summary>
+        /// <remarks>
+        /// Requires sketch StandardFirmata.ino to run on the connected device.
+        /// </remarks>
+        [TestMethod]
+        public void FindSerialConnection_FirmataEnabled()
+        {
+            using (var arduinoConnection = SerialConnection.FindSerialConnection())
+            {
+                Assert.IsNotNull(arduinoConnection);
+            }
+        }
+
+        /// <summary>
+        /// Finds a live serial connection by issuing a query string.
+        /// </summary>
+        /// <remarks>
+        /// Requires sketch SerialReply.ino to run on the connected device.
+        /// (This sketch can be found in this project.)
+        /// </remarks>
+        [TestMethod]
+        public void FindSerialConnection_Serial()
+        {
+            using (var arduinoConnection = SerialConnection.FindSerialConnection("Hello?", "Arduino!"))
+            {
+                Assert.IsNotNull(arduinoConnection);
+                Assert.AreEqual(9600, arduinoConnection.BaudRate);
+            }
+        }
+    }
+}
---
+++ Solid.Arduino.IntegrationTest/SerialReply.ino
@@ -0,0 +1,37 @@
+﻿/*
+   This example is intended to demonstrate the SolidSoils4Arduino library's
+   SerialConnection.FindSerialConnection(string query, string expectedReply) method.
+   
+   Created February 11th, 2015
+   by Henk van Boeijen. (info@solidsoils.nl)
+ 
+   See https://github.com/SolidSoils/Arduino
+   
+   This example code is in the public domain.
+ */
+
+#include <Firmata.h>
+
+char query[] = "Hello?";
+char reply[] = "Arduino!";
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial) {}
+}
+
+void loop()
+{
+  if (Serial.find(query))
+  {
+    Serial.println(reply);
+  }
+  else
+  {
+    Serial.println("Listening...");
+    Serial.flush();
+  }
+  
+  delay(25);
+}
